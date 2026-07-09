c196f3f20c8f4678493811e7f5f3e9fc7fe8d1e6
diya3108 <ambaliyadiya51@gmail.com>
2026-07-09 14:52:34 +0530

    Add files via upload

---
+++ introduction.java
@@ -0,0 +1,18 @@
+// Write a java program for introduction.
+
+
+
+public class introduction
+{
+	public static void main(String args[])
+	{
+		System.out.println("My name is Diya Ambaliya.");
+		System.out.println("I am studying in the second year of BCA.");
+		System.out.println("I like learning new skills every day.");
+		System.out.println("I am friendly and helpful person.");
+		System.out.println("My strength is that I never give up easily.");
+		
+	}
+	
+}
+	
